48d5e7caf43056df6d53037353fb283bd02f76a8
samihaAbdel <samKallel@hotmail.fr>
2024-09-07 16:36:31 +0100

    back

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+require("dotenv").config();
+const connectdb = require("./config/connectdb");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+const cors = require("cors");
+
+const app = express();
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+connectdb();
+//jwt
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
+
+//routes
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use((req, res) => {
+  res.send("API is running...")
+})
+
+// server
+PORT = process.env.PORT || 8100;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
